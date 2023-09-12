45be7eec194636e1f80a9fabbf777e7f46af4b56
Teemo Tebest <teemo.tebest@gmail.com>
2023-09-12 15:45:56 +0200

    Added new vis method

--- src/jsx/Pathway1.jsx
+++ src/jsx/Pathway1.jsx
@@ -1,5 +1,8 @@
-import React, { } from 'react';
+import React, { useEffect } from 'react';
 import '../styles/styles.less';
+
+// https://d3js.org/
+import * as d3 from 'd3';
 
 // https://www.npmjs.com/package/react-is-visible
 import 'intersection-observer';
@@ -10,6 +13,27 @@ import CountUp from 'react-countup';
 import easingFn from './helpers/EasingFn.js';
 
 function App() {
+  useEffect(() => {
+    const vis_container = d3.select('.dots');
+    vis_container.selectAll('span')
+      .style('height', 0)
+      .style('width', 0)
+      .style('opacity', 0)
+      .transition()
+      .duration(500)
+      .delay((d, i) => 30 * i)
+      .style('height', '50px')
+      .style('width', '50px')
+      .ease(d3.easeBounceOut)
+      .transition()
+      .duration(200)
+      .delay((d, i) => 30 * i)
+      .style('height', '30px')
+      .style('width', '30px')
+      .ease(d3.easeBounceOut)
+      .style('opacity', 1);
+  });
+
   return (
     <div className="app">
       <div className="container">
@@ -22,6 +46,33 @@ function App() {
                 <div className={`number number_1 ${(isVisible) ? 'visible' : ''}`}>$5.8 trillion</div>
                 <div className={`number number_2 ${(isVisible) ? 'visible' : ''}`}>$5.8 trillion</div>
                 <div className={`number number_3 ${(isVisible) ? 'visible' : ''}`}><CountUp easingFn={easingFn} end={300} duration={4} delay={6} separator="," useEasing /></div>
+                <div className="dots">
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span className="red" /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                  <div className="dot"><span /></div>
+                </div>
               </div>
             )}
           </IsVisible>
