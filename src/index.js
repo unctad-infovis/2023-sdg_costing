0c5e75a489b3fa9129039099cf771b5cc78f5dc1
Teemo Tebest <teemo.tebest@gmail.com>
2023-09-15 16:22:17 +0200

    Updated everything

--- src/index.js
+++ src/index.js
@@ -2,14 +2,130 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import Pathway1 from './jsx/Pathway1.jsx';
+import App from './jsx/App.jsx';
 
-import Pathway2 from './jsx/Pathway2.jsx';
-
+// Social protection and decent jobs
 const containerPathway1 = document.getElementById('app-root-2023-sdg_costing_pathway1');
-const rootPathway1 = createRoot(containerPathway1);
-rootPathway1.render(<Pathway1 />);
+if (containerPathway1) {
+  const rootPathway1 = createRoot(containerPathway1);
+  rootPathway1.render(
+    <App
+      gap={293.7}
+      gap_explaner="The eight small island developing states in the study face the largest relative gap at $3.3 billion."
+      per_person={1179}
+      percentage_min={1}
+      percentage_rest={74}
+      share_gdp={17.4}
+      total_cost={5386}
+      url="/sdg-costing/social-protection"
+    />
+  );
+}
 
+// Education transformation
 const containerPathway2 = document.getElementById('app-root-2023-sdg_costing_pathway2');
-const rootPathway2 = createRoot(containerPathway2);
-rootPathway2.render(<Pathway2 />);
+if (containerPathway2) {
+  const rootPathway2 = createRoot(containerPathway2);
+  rootPathway2.render(
+    <App
+      gap={274.5}
+      gap_explaner="The six least developed countries face the largest relative gap at $8.1 billion."
+      per_person={1300}
+      percentage_min={1}
+      percentage_rest={91}
+      share_gdp={19.1}
+      total_cost={5937}
+      url="/sdg-costing/education-transformation"
+    />
+  );
+}
+
+// Food systems
+const containerPathway3 = document.getElementById('app-root-2023-sdg_costing_pathway3');
+if (containerPathway3) {
+  const rootPathway3 = createRoot(containerPathway3);
+  rootPathway3.render(
+    <App
+      gap={327.8}
+      gap_explaner="The 29 upper-middle and high-income economies in the study face the largest relative annual gap at $309 billion."
+      per_person={1342}
+      percentage_min={1}
+      percentage_rest={98}
+      share_gdp={19.8}
+      total_cost={6127.2}
+      url="/sdg-costing/food-systems"
+    />
+  );
+}
+
+// Climate change, biodiversity loss, and pollution
+const containerPathway4 = document.getElementById('app-root-2023-sdg_costing_pathway4');
+if (containerPathway4) {
+  const rootPathway4 = createRoot(containerPathway4);
+  rootPathway4.render(
+    <App
+      gap={352.3}
+      gap_explaner="The 29 upper-middle and high-income developing economies in the study face the largest relative gap at $327 billion."
+      per_person={1251}
+      percentage_min={1}
+      percentage_rest={85}
+      share_gdp={18.5}
+      total_cost={5753.7}
+      url="/sdg-costing/climate-change"
+    />
+  );
+}
+
+// Energy
+const containerPathway5 = document.getElementById('app-root-2023-sdg_costing_pathway5');
+if (containerPathway5) {
+  const rootPathway5 = createRoot(containerPathway5);
+  rootPathway5.render(
+    <App
+      gap={286.1}
+      gap_explaner="The 29 upper-middle and high-income countries in the study face the largest relative gap at $281 billion."
+      per_person={1271}
+      percentage_min={1}
+      percentage_rest={87}
+      share_gdp={18.7}
+      total_cost={5804.7}
+      url="/sdg-costing/energy-transition"
+    />
+  );
+}
+
+// Inclusive digitalization
+const containerPathway6 = document.getElementById('app-root-2023-sdg_costing_pathway6');
+if (containerPathway6) {
+  const rootPathway6 = createRoot(containerPathway6);
+  rootPathway6.render(
+    <App
+      gap={468.6}
+      gap_explaner="The 29 upper-middle and high-income developing economies in the study face the largest relative gap at $430 billion."
+      per_person={1231}
+      percentage_min={1}
+      percentage_rest={81}
+      share_gdp={18.1}
+      total_cost={5620}
+      url="/sdg-costing/digitalization"
+    />
+  );
+}
+
+// Gender equality
+const containerPathway7 = document.getElementById('app-root-2023-sdg_costing_pathway7');
+if (containerPathway7) {
+  const rootPathway7 = createRoot(containerPathway7);
+  rootPathway7.render(
+    <App
+      gap={360.4}
+      gap_explaner="The eight small island developing states face the largest relative gap at $14.9 billion."
+      per_person={1383}
+      percentage_min={2}
+      percentage_rest={5}
+      share_gdp={20.5}
+      total_cost={6364}
+      url="/sdg-costing/gender-equality"
+    />
+  );
+}
---
+++ src/jsx/App.jsx
@@ -0,0 +1,159 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import '../styles/styles.less';
+
+// https://d3js.org/
+import * as d3 from 'd3';
+
+// https://www.npmjs.com/package/react-is-visible
+import 'intersection-observer';
+import IsVisible from 'react-is-visible';
+
+// https://www.npmjs.com/package/react-countup
+import CountUp from 'react-countup';
+
+// https://www.npmjs.com/package/uuid
+import { v4 as uuidv4 } from 'uuid';
+
+// Load helpers
+import easingFn from './helpers/EasingFn.js';
+import formatNr from './helpers/FormatNr.js';
+import roundNr from './helpers/RoundNr.js';
+
+function App({
+  gap,
+  gap_explaner,
+  per_person,
+  percentage_min,
+  percentage_rest,
+  share_gdp,
+  total_cost,
+  url
+}) {
+  const appRef = useRef();
+
+  const visualiseDots = () => {
+    const vis_container = d3.select(appRef.current);
+    setTimeout(() => {
+      vis_container.selectAll('.dot_wrapper')
+        .style('height', 0)
+        .style('width', 0)
+        .style('opacity', 0)
+        .transition()
+        .duration(500)
+        .delay((d, i) => 5 * i)
+        .style('height', '2px')
+        .style('width', '2px')
+        .ease(d3.easeBounceOut)
+        .transition()
+        .duration(200)
+        .delay((d, i) => 100 * i)
+        .style('height', '35px')
+        .style('width', '35px')
+        .ease(d3.easeBounceOut)
+        .style('opacity', 1);
+    }, 1300);
+  };
+  return (
+    <div className="app" ref={appRef}>
+      <div className="container">
+        <div className="background_ring" />
+        <div className="content">
+          <h2>How much it costs</h2>
+          <div className="intro">Figures include data for 48 developing economies</div>
+          <IsVisible once>
+            {(isVisible) => (
+              <>
+                <div className={`number_container left total_cost ${(isVisible) ? 'visible' : ''}`}>
+                  <div className="number total_cost">
+                    <span className="value">
+                      $
+                      {roundNr(total_cost / 1000, 1)}
+                    </span>
+                    <span className="unit">
+                      trillion
+                      <br />
+                      annually
+                    </span>
+                  </div>
+                  <div className="extra">
+                    This translates to
+                    {' '}
+                    $
+                    {formatNr(roundNr(per_person, 0))}
+                    {' '}
+                    per person
+                  </div>
+                </div>
+                <div className={`number_container right share_gdp ${(isVisible) ? 'visible' : ''}`}>
+                  <h3 className="share_gdp">Financial burden</h3>
+                  <div className="dots_container">
+                    <div className="dots">
+                      {
+                        Array(parseInt(10, 10)).fill().map((el, i) => <div key={uuidv4()} className="dot"><span className={`dot_wrapper ${(i < (percentage_min + 1)) ? 'share' : ''}`}><span className={(isVisible) ? 'visible' : ''} style={{ width: (i === percentage_min) ? `${percentage_rest}%` : '100%' }} /></span></div>)
+
+                      }
+                      {
+                        isVisible && visualiseDots()
+                      }
+                    </div>
+                    <div className="number share_gdp">
+                      <span className="value"><CountUp easingFn={easingFn} end={share_gdp} decimals={0} duration={2.5} delay={1.5} separator="," suffix="%" useEasing /></span>
+                      <span className="unit">
+                        of their
+                        <br />
+                        collective GDP
+                      </span>
+                    </div>
+                    <div className="extra" />
+                  </div>
+                </div>
+                <div className={`number_container left gap ${(isVisible) ? 'visible' : ''}`}>
+                  <h3 className="gap">Spending gap</h3>
+                  <div className="number gap">
+                    <span className="value">
+                      $
+                      {roundNr(gap, 0)}
+                    </span>
+                    <span className="unit">
+                      billion
+                      <br />
+                      annually
+                    </span>
+                  </div>
+                  <div className="extra">{gap_explaner}</div>
+                </div>
+                <div className={`show_more_container ${(isVisible) ? 'visible' : ''}`}>
+                  <div className="show_more_wrapper">
+                    <a type="button" href={url}>
+                      Learn
+                      <br />
+                      more
+                    </a>
+                  </div>
+                </div>
+              </>
+            )}
+          </IsVisible>
+        </div>
+      </div>
+      <noscript>Your browser does not support JavaScript!</noscript>
+    </div>
+  );
+}
+
+App.propTypes = {
+  gap: PropTypes.number.isRequired,
+  gap_explaner: PropTypes.string.isRequired,
+  per_person: PropTypes.number.isRequired,
+  percentage_min: PropTypes.number.isRequired,
+  percentage_rest: PropTypes.number.isRequired,
+  share_gdp: PropTypes.number.isRequired,
+  total_cost: PropTypes.number.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+};
+
+export default App;
