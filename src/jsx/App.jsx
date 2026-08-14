c9e204403bdff92b10754ad31de84ad58c755a67
Teemo Tebest <teemo.tebest@gmail.com>
2026-08-14 12:23:39 +0300

    Fix findDOMNode deprecation warning (convert IsVisible render-prop to useIsVisible hook)

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -7,7 +7,7 @@ import * as d3 from 'd3';
 
 // https://www.npmjs.com/package/react-is-visible
 import 'intersection-observer';
-import IsVisible from 'react-is-visible';
+import { useIsVisible } from 'react-is-visible';
 
 // https://www.npmjs.com/package/react-countup
 import CountUp from 'react-countup';
@@ -31,6 +31,8 @@ function App({
   total_cost,
   url
 }) {
+  const isVisibleRef1 = useRef();
+  const isVisible1 = useIsVisible(isVisibleRef1, { once: true });
   const appRef = useRef();
 
   const visualiseDots = () => {
@@ -62,79 +64,75 @@ function App({
         <div className="content">
           <h2>How much will it cost?</h2>
           <div className="intro">Figures include data for 48 developing economies</div>
-          <IsVisible once>
-            {(isVisible) => (
-              <>
-                <div className={`number_container left total_cost ${(isVisible) ? 'visible' : ''}`}>
-                  <div className="number total_cost">
-                    <span className="value">
-                      $
-                      {roundNr(total_cost / 1000, 1)}
-                    </span>
-                    <span className="unit">
-                      trillion
-                      <br />
-                      annually
-                    </span>
-                  </div>
-                  <div className="extra">
-                    This translates to
-                    {' '}
-                    $
-                    {formatNr(roundNr(per_person, 0))}
-                    {' '}
-                    per person
-                  </div>
-                </div>
-                <div className={`number_container right share_gdp ${(isVisible) ? 'visible' : ''}`}>
-                  <h3 className="share_gdp">Financial burden</h3>
-                  <div className="dots_container">
-                    <div className="dots">
-                      {
-                        Array(parseInt(10, 10)).fill().map((el, i) => <div key={uuidv4()} className="dot"><span className={`dot_wrapper ${(i < (percentage_min + 1)) ? 'share' : ''}`}><span className={(isVisible) ? 'visible' : ''} style={{ width: (i === percentage_min) ? `${percentage_rest}%` : '100%' }} /></span></div>)
+
+          <div ref={isVisibleRef1} className={`number_container left total_cost ${(isVisible1) ? 'visible' : ''}`}>
+            <div className="number total_cost">
+              <span className="value">
+                $
+                {roundNr(total_cost / 1000, 1)}
+              </span>
+              <span className="unit">
+                trillion
+                <br />
+                annually
+              </span>
+            </div>
+            <div className="extra">
+              This translates to
+              {' '}
+              $
+              {formatNr(roundNr(per_person, 0))}
+              {' '}
+              per person
+            </div>
+          </div>
+          <div className={`number_container right share_gdp ${(isVisible1) ? 'visible' : ''}`}>
+            <h3 className="share_gdp">Financial burden</h3>
+            <div className="dots_container">
+              <div className="dots">
+                {
+                        Array(parseInt(10, 10)).fill().map((el, i) => <div key={uuidv4()} className="dot"><span className={`dot_wrapper ${(i < (percentage_min + 1)) ? 'share' : ''}`}><span className={(isVisible1) ? 'visible' : ''} style={{ width: (i === percentage_min) ? `${percentage_rest}%` : '100%' }} /></span></div>)
                       }
-                      {
-                        isVisible && visualiseDots()
+                {
+                        isVisible1 && visualiseDots()
                       }
-                    </div>
-                    <div className="number share_gdp">
-                      <span className="value"><CountUp easingFn={easingFn} end={share_gdp} decimals={0} duration={2.5} delay={1.5} separator="," suffix="%" useEasing /></span>
-                      <span className="unit">
-                        of their
-                        <br />
-                        collective GDP
-                      </span>
-                    </div>
-                    <div className="extra">{share_gdp_explaner}</div>
-                  </div>
-                </div>
-                <div className={`number_container left gap ${(isVisible) ? 'visible' : ''}`}>
-                  <h3 className="gap">Spending gap</h3>
-                  <div className="number gap">
-                    <span className="value">
-                      $
-                      {roundNr(gap, 0)}
-                    </span>
-                    <span className="unit">
-                      billion
-                      <br />
-                      annually
-                    </span>
-                  </div>
-                  <div className="extra">{gap_explaner}</div>
-                </div>
-                <div className={`show_more_container ${(isVisible) ? 'visible' : ''}`}>
-                  <div className="show_more_wrapper">
-                    <a type="button" href={url}>
-                      Learn
-                      <br />
-                      more
-                    </a>
-                  </div>
-                </div>
-              </>
-            )}
-          </IsVisible>
+              </div>
+              <div className="number share_gdp">
+                <span className="value"><CountUp easingFn={easingFn} end={share_gdp} decimals={0} duration={2.5} delay={1.5} separator="," suffix="%" useEasing /></span>
+                <span className="unit">
+                  of their
+                  <br />
+                  collective GDP
+                </span>
+              </div>
+              <div className="extra">{share_gdp_explaner}</div>
+            </div>
+          </div>
+          <div className={`number_container left gap ${(isVisible1) ? 'visible' : ''}`}>
+            <h3 className="gap">Spending gap</h3>
+            <div className="number gap">
+              <span className="value">
+                $
+                {roundNr(gap, 0)}
+              </span>
+              <span className="unit">
+                billion
+                <br />
+                annually
+              </span>
+            </div>
+            <div className="extra">{gap_explaner}</div>
+          </div>
+          <div className={`show_more_container ${(isVisible1) ? 'visible' : ''}`}>
+            <div className="show_more_wrapper">
+              <a type="button" href={url}>
+                Learn
+                <br />
+                more
+              </a>
+            </div>
+          </div>
+
         </div>
       </div>
       <noscript>Your browser does not support JavaScript!</noscript>
